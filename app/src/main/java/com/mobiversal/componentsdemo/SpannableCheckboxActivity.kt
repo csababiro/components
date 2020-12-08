d0aa92cb3bff05bfccff5f4f466060323006b6ca
Csaba <csaba@mobiversal.com>
2020-12-08 15:15:41 +0200

    spannable checkbox

--- app/src/main/java/com/mobiversal/componentsdemo/SpannableCheckboxActivity.kt
+++ app/src/main/java/com/mobiversal/componentsdemo/SpannableCheckboxActivity.kt
@@ -1,6 +1,11 @@
 package com.mobiversal.componentsdemo
 
 import android.os.Bundle
+import com.mobiversal.kotlinextensions.logs.logDebug
+import kotlinx.android.synthetic.main.activity_spannable_checkbox.*
+
+private const val TERMS = 0
+private const val PRIVACY = 1
 
 class SpannableCheckboxActivity : ParentActivity() {
 
@@ -8,5 +13,27 @@ class SpannableCheckboxActivity : ParentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spannable_checkbox)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        displayAgreeText()
+    }
+
+    private fun displayAgreeText() {
+        cbAgree.displaySpannableText(R.string.i_agree_with_terms, R.array.terms)
+        cbAgree.setSpannableClickListener { index -> agreeTermsClicked(index) }
+    }
+
+    private fun agreeTermsClicked(index: Int) {
+        when (index) {
+            TERMS -> termsClicked()
+            PRIVACY -> privacyClicked()
+        }
+    }
+
+    private fun termsClicked() {
+        logDebug("TERMS", "Terms clicked")
+    }
+
+    private fun privacyClicked() {
+        logDebug("PRIVACY", "Privacy clicked")
     }
 }
